06963c7de3dac136e73325404029f726a269549f
ErJump <fedelijunior@gmail.com>
2022-06-15 14:52:07 +0200

    Add aesthetic and reset button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 //Dichiaro button riprendendo tramite id
 const button = document.getElementById("submit-button");
-
+const buttonReset = document.getElementById("reset-button");
 //dichiaro l'evento basato su click del bottone
 button.addEventListener('click', function(){
     //dichiaro le variabili chiedendo all'utente età e km tramite i tag input
     const userAge = parseInt(document.getElementById("input-user-age").value);
     const userTravel = parseInt(document.getElementById("input-user-km").value);
     //calcolo il prezzo base
-    let normalPrice = (userTravel * 0.26).toFixed(2);
+    const normalPrice = (userTravel * 0.26).toFixed(2);
     let result;
+    console.log({userAge});
     //casi per età
     if (userAge < 18){
         //nel caso di minorenni viene applicato un 15% di sconto
@@ -22,4 +23,10 @@ button.addEventListener('click', function(){
         //altrimenti non viene applicato sconto e mostrato il prezzo di base
         document.getElementById("result").innerHTML = `Il prezzo del tuo biglietto è ${normalPrice}€.`;
     }
+});
+
+buttonReset.addEventListener('click', function(){
+    document.getElementById("input-user-age").value = null;
+    document.getElementById("input-user-km").value = null;
+    document.getElementById("input-user-nominative").value = null;
 });
